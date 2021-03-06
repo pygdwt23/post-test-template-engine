2d7edf213473065a42c60f7a22334cbd3f3666f5
Prayogo Dewantoro <pygdwt@gmail.com>
2021-03-06 09:05:41 +0800

    [Post Test] Done!

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Homepage" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/news", (req, res) => {
+  const url = "https://berita-indo-api.vercel.app/v1";
+
+  axios.get(`${url}/cnbc-news`).then((result) => {
+    res.render("news", { title: "Berita Hari Ini", news: result.data.data });
+  });
+});
+
+app.get("/corona", (req, res) => {
+  const url = "https://api.kawalcorona.com/indonesia/provinsi/";
+  axios.get(`${url}`).then((result) => {
+    res.render("corona", { title: "Data Corona", corona: result.data });
+  });
+});
+
+app.get("/greet", (req, res) => {
+  res.render("greet", { name: req.query.name });
+});
+
+app.listen(2500, () => {
+  console.log("Server is up and running on port 2500");
+});
